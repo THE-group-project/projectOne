135cec94f950eed95541084b049565dec4e20755
ogesour <ogesour@gmail.com>
2018-11-17 10:55:54 -0800

    Almost there!

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAA5pfTDwiagC2B-wWw67H1LSlrSf8meFY",
+    authDomain: "bbbproject-b637f.firebaseapp.com",
+    databaseURL: "https://bbbproject-b637f.firebaseio.com",
+    projectId: "bbbproject-b637f",
+    storageBucket: "bbbproject-b637f.appspot.com",
+    messagingSenderId: "221102547170"
+  };
+  firebase.initializeApp(config);
+
+ 
